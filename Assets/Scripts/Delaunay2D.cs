57db57828b2bad394e7532cab4d887d81a6e6c78
BoDeKeersmaeker <bo.de.keersmaeker@student.howest.be>
2022-01-21 03:32:05 +0100

    Added dalaunay2D algorithm
    Added dalaunay2D algorithm not testen yet.

--- Assets/Scripts/Delaunay2D.cs
+++ Assets/Scripts/Delaunay2D.cs
@@ -9,6 +9,7 @@ public class Delaunay2D : BaseAlgorithm
         private Vector2 VertorX;
         private Vector2 VertorY;
         private Vector2 VertorZ;
+        public bool IsValid {get; set;}
 
         public Triangle(Vector2 vertorX, Vector2 vertorY, Vector2 vertorZ)
         {
@@ -16,11 +17,93 @@ public class Delaunay2D : BaseAlgorithm
             VertorY = vertorY;
             VertorZ = vertorZ;
         }
+        public Vector2 GetVectorX()
+        {
+            return VertorX;
+        }
+        public Vector2 GetVectorY()
+        {
+            return VertorY;
+        }
+        public Vector2 GetVectorZ()
+        {
+            return VertorZ;
+        }
+        public bool SharesVector(Triangle triangle)
+        {
+            if (Vector3.Distance(VertorX, triangle.GetVectorX()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorY, triangle.GetVectorX()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorZ, triangle.GetVectorX()) < 0.01f)
+                return true;
+
+            if (Vector3.Distance(VertorX, triangle.GetVectorY()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorY, triangle.GetVectorY()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorZ, triangle.GetVectorY()) < 0.01f)
+                return true;
+
+            if (Vector3.Distance(VertorX, triangle.GetVectorZ()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorY, triangle.GetVectorZ()) < 0.01f)
+                return true;
+            if (Vector3.Distance(VertorZ, triangle.GetVectorZ()) < 0.01f)
+                return true;
+
+            return false;
+        }
+        public bool IsPointInCircumCircle(Vector2 v)
+        {
+            float ab = VertorX.sqrMagnitude;
+            float cd = VertorY.sqrMagnitude;
+            float ef = VertorZ.sqrMagnitude;
+
+            float circumX = (ab * (VertorZ.y - VertorY.y) + cd * (VertorX.y - VertorZ.y) + ef * (VertorY.y - VertorX.y)) / (VertorX.x * (VertorZ.y - VertorY.y) + VertorY.x * (VertorX.y - VertorZ.y) + VertorZ.x * (VertorY.y - VertorX.y));
+            float circumY = (ab * (VertorZ.x - VertorY.x) + cd * (VertorX.x - VertorZ.x) + ef * (VertorY.x - VertorX.x)) / (VertorX.y * (VertorZ.x - VertorY.x) + VertorY.y * (VertorX.x - VertorZ.x) + VertorZ.y * (VertorY.x - VertorX.x));
+
+            Vector2 circum = new Vector2(circumX / 2f, circumY / 2f);
+            float circumRadius = Vector2.SqrMagnitude(VertorX - circum);
+            float dist = Vector2.SqrMagnitude(v - circum);
+            return dist <= circumRadius;
+        }
+    }
+
+    public class Line
+    {
+        private Vector2 VertorA;
+        private Vector2 VertorB;
+        public bool IsValid { get; set; }
+
+        public Line(Vector2 vertorA, Vector2 vertorB)
+        {
+            VertorA = vertorA;
+            VertorB = vertorB;
+        }
+        public Vector2 GetVectorA()
+        {
+            return VertorA;
+        }
+        public Vector2 GetVectorB()
+        {
+            return VertorB;
+        }
+        public bool Equals(Line Line)
+        {
+            if (VertorA == Line.GetVectorA() && VertorB == Line.GetVectorB())
+                return true;
+            if (VertorA == Line.GetVectorB() && VertorB == Line.GetVectorA())
+                return true;
+
+            return false;
+        }
     }
 
     private List<Vector3> Vertices = null;
     private List<Triangle> Triangles = null;
-    
+    private List<Line> Lines = null;
+    private Triangle SuperTriangle = null;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +121,7 @@ public class Delaunay2D : BaseAlgorithm
     {
         initializeVertices(rooms);
         CalculateSuperTriangle();
+        AddPointToTriangulation();
     }
 
     public void initializeVertices(GameObject[,,] rooms)
@@ -45,8 +129,9 @@ public class Delaunay2D : BaseAlgorithm
         foreach(GameObject ro in rooms)
             Vertices.Add(ro.transform.position);
     }
-
-    public void CalculateSuperTriangle()
+    
+    //rework later
+    private void CalculateSuperTriangle()
     {
         float minX = Vertices[0].x;
         float minY = Vertices[0].z;
@@ -74,5 +159,62 @@ public class Delaunay2D : BaseAlgorithm
         Vector2 VertorZ = new Vector2(maxX + deltaMax, minY - 1);
 
         Triangles.Add(new Triangle(VertorX, VertorY, VertorZ));
+    }
+
+    private void AddPointToTriangulation()
+    {
+        foreach (Vector3 vertex in Vertices)
+        {
+            List<Line> line = new List<Line>();
+
+            foreach (Triangle triangle in Triangles)
+            {
+                if (triangle.IsPointInCircumCircle(vertex))
+                {
+                    triangle.IsValid = false;
+                    line.Add(new Line(triangle.GetVectorX(), triangle.GetVectorY()));
+                    line.Add(new Line(triangle.GetVectorY(), triangle.GetVectorZ()));
+                    line.Add(new Line(triangle.GetVectorZ(), triangle.GetVectorX()));
+                }
+            }
+
+            Triangles.RemoveAll((Triangle t) => !t.IsValid);
+
+            for (int i = 0; i < line.Count; i++)
+            {
+                for (int j = i + 1; j < line.Count; j++)
+                {
+                    if (line[i].Equals(line[j]))
+                    {
+                        line[i].IsValid = false;
+                        line[j].IsValid = false;
+                    }
+                }
+            }
+
+            line.RemoveAll((Line e) => e.IsValid);
+
+            foreach (Line temp in line)
+                Triangles.Add(new Triangle(temp.GetVectorA(), temp.GetVectorB(), vertex));
+        }
+
+        Triangles.RemoveAll((Triangle triangle) => triangle.SharesVector(SuperTriangle));
+
+        HashSet<Line> lines = new HashSet<Line>();
+
+        foreach (Triangle triangle in Triangles)
+        {
+            Line line = new Line(triangle.GetVectorX(), triangle.GetVectorY());
+            if (lines.Add(line))
+                Lines.Add(line);
+
+            line = new Line(triangle.GetVectorY(), triangle.GetVectorZ());
+            if (lines.Add(line))
+                Lines.Add(line);
+
+            line = new Line(triangle.GetVectorZ(), triangle.GetVectorX());
+            if (lines.Add(line))
+                Lines.Add(line);
+        }
     }
 }
